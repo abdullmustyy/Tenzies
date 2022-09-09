16e78be7de03682c81b82275f626eb6b16fd46da
abdullmustyy <abdulmusty03@outlook.com>
2022-09-09 03:45:32 +0100

    Initil commit

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <span className="header--title">Tenzie</span>
+            <img src="../public/vite.svg" alt="Vite Logo" className="header--logo" />
+        </header>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import Header from "../components/Header";
+
+export default function App() {
+  return (
+    <section className="section">
+      <Header />
+      <div className="tenzie">
+        <div className="tenzie--cont">
+          <h1>hi</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
