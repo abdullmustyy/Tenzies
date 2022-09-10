70ae6875cef797ccbdf15a2969d61ba57f0ac1bf
abdullmustyy <abdulmusty03@outlook.com>
2022-09-10 20:43:56 +0100

    Add functionality

--- components/Dice.jsx
+++ components/Dice.jsx
@@ -1,12 +1,27 @@
-export default function Dice(props) {
-  const diceElements = props.allDice.map((dice) => {
-    return <div className="tenzie--die">{dice.value}</div>;
+export default function Dice({ allDice, holdDie, rollUnheldDie, tenzies }) {
+  const diceElements = allDice.map((die) => {
+    const styles = {
+      backgroundColor: die.held ? "#5035FF" : "#cdc8f2",
+      color: die.held ? "#ddd9f7" : "#2B283A",
+    };
+
+    return (
+      <div
+        className="tenzie--die"
+        onClick={() => holdDie(die.id)}
+        style={styles}
+      >
+        <h3 className="tenzie--die-no">{die.value}</h3>
+      </div>
+    );
   });
 
   return (
     <section className="tenzie--dice-section">
       <div className="tenzie--grid">{diceElements}</div>
-      <button className="tenzie--btn">Roll</button>
+      <button className="tenzie--btn" onClick={rollUnheldDie}>
+        {tenzies ? "Reset Game" : "Roll"}
+      </button>
     </section>
   );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,54 @@ import Header from "../components/Header";
 import Dice from "../components/Dice";
 
 export default function App() {
-  const [allDice, setAllDice] = useState(allDiceAray);
+  const [allDice, setAllDice] = useState(allDiceAray());
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const firstDieValue = allDice[0].value;
+    const allHeld = allDice.every((die) => die.held);
+    const allSameNumber = allDice.every((die) => die.value === firstDieValue);
+    if (allHeld && allSameNumber) {
+      setTenzies((prevTenzies) => !prevTenzies);
+    }
+  }, [allDice]);
+
+  function randomDieNumber() {
+    return Math.ceil(Math.random() * 6);
+  }
 
   function allDiceAray() {
     const diceArray = [];
     for (let i = 0; i < 10; i++) {
-      diceArray.push({
-        id: i,
-        value: i + 1,
-      });
+      const newDie = {
+        id: i + 1,
+        value: randomDieNumber(),
+        held: false,
+      };
+      diceArray.push(newDie);
     }
     return diceArray;
+  }
+
+  function holdDie(id) {
+    setAllDice((prevDiceArray) =>
+      prevDiceArray.map((die) =>
+        die.id === id ? { ...die, held: !die.held } : die
+      )
+    );
+  }
+
+  function rollUnheldDie() {
+    if (tenzies) {
+      setAllDice(allDiceAray());
+      setTenzies((prevTenzies) => !prevTenzies);
+    } else {
+      setAllDice((prevDiceArray) =>
+        prevDiceArray.map((die) =>
+          die.held ? die : { ...die, value: randomDieNumber() }
+        )
+      );
+    }
   }
 
   return (
@@ -26,10 +63,15 @@ export default function App() {
             Roll until all dice are the same. Click each die to freeze it at its
             current value between rolls.
           </span>
-          <Dice allDice={allDice} />
+          <Dice
+            allDice={allDice}
+            holdDie={holdDie}
+            rollUnheldDie={rollUnheldDie}
+            tenzies={tenzies}
+          />
         </div>
       </div>
-      <p className="footer--text">Built with ❤️ in Naij.</p>
+      <p className="footer--text">Built with 💟 in Naij.</p>
     </section>
   );
 }
