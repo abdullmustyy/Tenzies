6c2b77cc63921a08fad0c8cdabce19d3d4b216b1
abdullmustyy <abdulmusty03@outlook.com>
2022-09-11 20:37:04 +0100

    Fix routing

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <header className="header">
             <span className="header--title">Tenzies</span>
-            <img src="../src/assets/dice.png" alt="Tenzies Logo" className="header--logo" />
+            <img src="/src/assets/dice.png" alt="Tenzies Logo" className="header--logo" />
         </header>
     )
 }
